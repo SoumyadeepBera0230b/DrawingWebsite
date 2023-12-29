1df72dac880457b86d36a8f495b5117f8ac48822
Soumyadeep Bera <soumyadeep0230b@gmail.com>
2023-12-30 05:01:57 +0530

    Updates

---
+++ precache-manifest.5a0a7ecbbc3f5d2a7cf05b8d73d51d67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a118001f1d34c109e82ed9e9fe302ab",
+    "url": "/DrawingWebsite/index.html"
+  },
+  {
+    "revision": "20fb69224461b7a69091",
+    "url": "/DrawingWebsite/static/css/main.e14d4fa2.chunk.css"
+  },
+  {
+    "revision": "8bc4e4354f9776f7e158",
+    "url": "/DrawingWebsite/static/js/2.053117a9.chunk.js"
+  },
+  {
+    "revision": "73d85020198a4e76ee18b2345cc73f86",
+    "url": "/DrawingWebsite/static/js/2.053117a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fb69224461b7a69091",
+    "url": "/DrawingWebsite/static/js/main.fb9645f7.chunk.js"
+  },
+  {
+    "revision": "42b02210c587099c7f4a",
+    "url": "/DrawingWebsite/static/js/runtime-main.871675d5.js"
+  },
+  {
+    "revision": "1d0eb6ee633cdcccd5e9695319d9df0d",
+    "url": "/DrawingWebsite/static/media/img1.1d0eb6ee.jpg"
+  },
+  {
+    "revision": "e20ddd37907aa57173ac6f07ea6d84f1",
+    "url": "/DrawingWebsite/static/media/naruto.e20ddd37.jpg"
+  },
+  {
+    "revision": "bc44f80fb6cc3d119926b236af711c90",
+    "url": "/DrawingWebsite/static/media/pinterest.bc44f80f.png"
+  },
+  {
+    "revision": "450dc3fbead48b39cb15999a9c75ad1d",
+    "url": "/DrawingWebsite/static/media/whatsapp.450dc3fb.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/DrawingWebsite/precache-manifest.5a0a7ecbbc3f5d2a7cf05b8d73d51d67.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DrawingWebsite/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
