04d4dac6f9290d4de3bf3b5c92a266e654975adc
Soumyadeep Bera <soumyadeep0230b@gmail.com>
2023-12-30 04:09:20 +0530

    build is committed

---
+++ build/precache-manifest.e0f642bdf6a876bb75e4401c286881e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25061ba31082b6629696076c5f6d0739",
+    "url": "/pencilmaniaArtsWebsite/index.html"
+  },
+  {
+    "revision": "20fb69224461b7a69091",
+    "url": "/pencilmaniaArtsWebsite/static/css/main.e14d4fa2.chunk.css"
+  },
+  {
+    "revision": "8bc4e4354f9776f7e158",
+    "url": "/pencilmaniaArtsWebsite/static/js/2.053117a9.chunk.js"
+  },
+  {
+    "revision": "73d85020198a4e76ee18b2345cc73f86",
+    "url": "/pencilmaniaArtsWebsite/static/js/2.053117a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fb69224461b7a69091",
+    "url": "/pencilmaniaArtsWebsite/static/js/main.fb9645f7.chunk.js"
+  },
+  {
+    "revision": "15a6bcc0b2ac1a6f1a63",
+    "url": "/pencilmaniaArtsWebsite/static/js/runtime-main.c1ce2fd6.js"
+  },
+  {
+    "revision": "1d0eb6ee633cdcccd5e9695319d9df0d",
+    "url": "/pencilmaniaArtsWebsite/static/media/img1.1d0eb6ee.jpg"
+  },
+  {
+    "revision": "e20ddd37907aa57173ac6f07ea6d84f1",
+    "url": "/pencilmaniaArtsWebsite/static/media/naruto.e20ddd37.jpg"
+  },
+  {
+    "revision": "bc44f80fb6cc3d119926b236af711c90",
+    "url": "/pencilmaniaArtsWebsite/static/media/pinterest.bc44f80f.png"
+  },
+  {
+    "revision": "450dc3fbead48b39cb15999a9c75ad1d",
+    "url": "/pencilmaniaArtsWebsite/static/media/whatsapp.450dc3fb.jpeg"
+  }
+]);
